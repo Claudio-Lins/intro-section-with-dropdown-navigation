8c015f954de6003b16fb396838735fc8733b1676
Claudio Lins <claudio.lins@me.com>
2022-09-24 23:27:17 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
-import { CaretDown, List, X } from "phosphor-react";
 import { useState } from "react";
 import { Company, Features, Menus } from "../../data/menusMobile";
 import { Dropdown } from "./Dropdown";
